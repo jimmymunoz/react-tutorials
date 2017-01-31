d2074a81e9b7e5ed193857968c5381783aa7aef2
jimmymunoz <myappeu@gmail.com>
2017-01-31 08:32:14 +0100

    React init

---
+++ hello-world/src/Game.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Board from './Board';
+
+class Game extends React.Component {
+	render(){
+		return (
+			<div className="game">
+			  <div className="game-board">
+			  	<Board />
+			  </div>
+			  <div className="game-info">
+				<div>{}</div>
+				<div>{}</div>
+			  </div>
+			</div>
+		);
+	}
+}
+
+export default Game;
